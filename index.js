721a50fc2377da654275a2398c70fa6e34bd6047
sobuj301 <sobuj42301@gmail.com>
2023-11-18 10:19:12 +0600

    data storge to database and read client side

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 const express = require('express');
 const cors = require('cors')
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express()
 const port = process.env.PORT || 5000;
 
 
-// iXlabi0ScEEvlNq2
+
 app.use(cors())
 app.use(express.json())
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+
 const uri = "mongodb+srv://sobujrana43997:iXlabi0ScEEvlNq2@cluster0.udy85xl.mongodb.net/?retryWrites=true&w=majority";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -23,20 +24,48 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
+    
     await client.connect();
-    // Send a ping to confirm a successful connection
+    // const database = client.db("insertDB");
+    // const haiku = database.collection("haiku");
+
+      const brandsCollection = client.db('brandsDB').collection('brands')
+
+      app.post('/brands',async(req,res) => {
+        const car = req.body;
+        console.log(car)
+        const result = await brandsCollection.insertOne(car)
+        res.send(result)
+      })
+
+      app.get('/brands',async(req,res) =>{
+        const result =await brandsCollection.find().toArray()
+        res.send(result)
+      })
+
+
+    app.get('/brands/:name',async(req,res) =>{
+       const brand = req.params.name;
+       const query = {brand:brand};
+        const result =await brandsCollection.find(query).toArray()
+        res.send(result)
+      })
+
+    app.get('/brands/:id',async(req,res) =>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await brandsCollection.findOne(query)
+      res.send(result)
+    })
+
+          
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
+  
   }
 }
 run().catch(console.dir);
-
-
-
 
 app.get('/',(req,res) =>{
     res.send('Mercedes-car-dealerships server is running.............. ')
